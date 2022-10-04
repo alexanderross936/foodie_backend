a49be3ae2c455ee14e8bd2e8d62de48f4cb85bdb
Alex Ross <alexandeross936@gmail.com>
2022-10-04 12:34:59 -0500

    cors change

--- index.js
+++ index.js
@@ -11,21 +11,21 @@ const HttpError = require('./models/http-error');
 const cors = require("cors");
 
 const app = express();
-
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
+
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   );
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
-  next();
-});
+//   next();
+// });
 
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
